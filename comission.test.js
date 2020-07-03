d36e9bec66ce27d4f3abb93f9e27560d4400fbe8
muhammad-114@mail.ru <aBA6DffCnXEN>
2020-07-03 21:00:18 +0300

    unit-testing homework-4

--- comission.test.js
+++ comission.test.js
@@ -1,33 +1,22 @@
 const {commission} = require("./commission");
 
 const now = Date.now();
-const hour = 60 * 60 * 1000
+const hour = 60 * 60 * 1000;
 const day = 24 * hour;
 const fiveDays = day * 5;
 const tenDays = day * 10;
 
-const test = (text, fn) => {
-  const color = fn() === true ? '\x1b[32m' : '\x1b[31m';
-  console.log(color, "✓", text);
-};
-
-test("Если бесконечно опоздал, то комиссия 100%", () => commission(now - Infinity) === 100);
-test("Если опоздал на 1 час, то комиссия 100%", () => commission(now - hour) === 100);
-test("Если вернул одновременно с вылетом, то комиссия 75%", () => commission(now) === 75);
-test("Если вернул за час до вылета, то комиссия 75%", () => commission(now + hour) === 75);
-test("Если вернул за 23 часа до вылета, то комиссия 75%", () => commission(now + hour * 23) === 75);
-test("Если вернул за сутки до вылета, то комиссия 50%", () => commission(now + day) === 50);
-test("Если вернул за 25 часов до вылета, то комиссия 50%", () => commission(now + hour * 25) === 50);
-test("Если вернул за 5 суток без одного часа до вылета, то комиссия 50%", () => commission(now + fiveDays - hour) === 50);
-test("Если вернул за 5 суток до вылета, то комиссия 20%", () => commission(now + fiveDays) === 20);
-test("Если вернул за 5 суток и один час до вылета, то комиссия 20%", () => commission(now + fiveDays + hour) === 20);
-test("Если вернул за 10 суток до вылета, то комиссия 20%", () => commission(now + tenDays) === 20);
-test("Если вернул за 10 суток и один час до вылета, то комиссия 0", () => commission(now + tenDays) === 0);
-test("Если бесконечно рано вернул, то комиссия 0", () => commission(now + Infinity) === 0);
-test("Если ввел в функцию строку, то получаю ошибку", () => {
-  try {
-    commission("5 дней")
-  } catch {
-    return true;
-  }
-});
+test("Если бесконечно опоздал, то комиссия 100%", () => expect(commission(now - Infinity)).toBe(100));
+test("Если опоздал на 1 час, то комиссия 100%", () => expect(commission(now - hour)).toBe(100));
+test("Если вернул одновременно с вылетом, то комиссия 75%", () => expect(commission(now)).toBe(75));
+test("Если вернул за час до вылета, то комиссия 75%", () => expect(commission(now + hour)).toBe(75));
+test("Если вернул за 23 часа до вылета, то комиссия 75%", () => expect(commission(now + hour * 23)).toBe(75));
+test("Если вернул за сутки до вылета, то комиссия 50%", () => expect(commission(now + day)).toBe(50));
+test("Если вернул за 25 часов до вылета, то комиссия 50%", () => expect(commission(now + hour * 25)).toBe(50));
+test("Если вернул за 5 суток без одного часа до вылета, то комиссия 50%", () => expect(commission(now + fiveDays - hour)).toBe(50));
+test("Если вернул за 5 суток до вылета, то комиссия 20%", () => expect(commission(now + fiveDays)).toBe(20));
+test("Если вернул за 5 суток и один час до вылета, то комиссия 20%", () => expect(commission(now + fiveDays + hour)).toBe(20));
+test("Если вернул за 10 суток до вылета, то комиссия 20%", () => expect(commission(now + tenDays)).toBe(20));
+test("Если вернул за 10 суток и один час до вылета, то комиссия 0", () => expect(commission(now + tenDays)).toBe(0));
+test("Если бесконечно рано вернул, то комиссия 0", () => expect(commission(now + Infinity)).toBe(0));
+test("Если ввел в функцию строку, то получаю ошибку", () => expect(() => commission("5 дней")).toThrow());
