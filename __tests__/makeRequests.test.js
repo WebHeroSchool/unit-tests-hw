e3fe3635447ef2a370e5d5228cdf7f454f06eb2e
Антон Ткачук <chiga2030@gmail.com>
2020-07-08 21:57:32 +0300

    test for makeRequests

--- __tests__/makeRequests.test.js
+++ __tests__/makeRequests.test.js
@@ -1,1 +1,17 @@
-import 
+const axios = require('../axios');
+
+
+
+
+	const p1 = Promise.resolve(3);
+	const p2 = 1337;
+	const p3 = new Promise(resolve => setTimeout(resolve, 1000, "foo"));
+	const promises = [p1, p2, p3];
+
+
+
+	test('описание', () => {
+	  return Promise.all(promises).then(data => {
+	    expect(data).toEqual([3, 1337, "foo"]);
+	  });
+	});
