17268923ffc4ee6582499e8872ae2c67b4c74987
Anton <chiga2030#gmail.com>
2020-07-21 08:54:25 +0300

    test rejectOnTimeot

--- __tests__/rejectOnTimeout.test.js
+++ __tests__/rejectOnTimeout.test.js
@@ -6,29 +6,43 @@ describe('Тестирование таймера', () => {
 	jest.useFakeTimers();
 	let promise;
 	let ms;
+	let testVal;
 
 	beforeEach(() => {
-		promise = new Promise((resolve, reject) => {
-			setTimeout(() => {
-				const response = 'foo';
-				resolve(response);
-			}, 2000);
-		});
+
+
+	promise = new Promise((resolve, reject) => {
+		if(true) {
+			const timeOut = setTimeout(() => resolve('foo'), 1000)
+		} else {
+			const reason = new Error('important error')
+			reject(reason)
+		}
+	})
+
 		
-		mockFnRes = jest.fn(() => promise);
+		//mockFnRes = jest.fn(() => promise);
 		//mockFnRej = jest.fn(() => reject('rej'));
 		ms = 4000;
-		rejectOnTimeout(mockFnRes, ms);
+		testVal = rejectOnTimeout(promise, ms);
 	});
 
 
-	test('desription', () => {
-	 // jest.runTimersToTime(5000);
-	  expect(mockFnRes).toEqual('foo');
+	test('test resolved', () => {
+		jest.runTimersToTime(3000);
+		
+	  	//expect(data).toEqual('foo');
+	  	return expect(testVal).resolves.toBe('foo');
+
+
 	  //expect(mockFn).toHaveBeenCalled();
 	  //expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 200);
 	});
 
+	test('test rejected', () => {
+		jest.runTimersToTime(5000);
+	  	return expect(testVal).rejects.toMatch('error');
+	});
 	// 	test('desription', () => {
 	// 	rejectOnTimeout(Promise.resolve(3), ms);
 	//   jest.runTimersToTime(3000);
