5b6039677d3408b7d29b6278d572cb44f742e5fe
Владимирова Екатерина <echapyrkina@mail.ru>
2021-03-31 18:56:48 +0400

    lesson 5

--- commission.test.js
+++ commission.test.js
@@ -1,15 +1,15 @@
 const commission = require('commission.js');
 const MILLISECONDS_IN_DAY = 24 * 60 * 60 * 1000;
-let flyDate = Date.now() + days_before_fly* MILLISECONDS_IN_DAY;
+let flyDate = Date.now() + days_before_fly * MILLISECONDS_IN_DAY;
 
 describe('возврат от 0 до 20%', () => {
 	test('время возврата > 10 суток', () => {
 		expect(commission(flyDate(10)).toBe(0));
 	});
-		test('время возврата = 10 суток', () => {
+	test('время возврата = 10 суток', () => {
 		expect(commission(flyDate(10)).toBe(20));
 	});
-		test('время возврата < 10 суток', () => {
+	test('время возврата < 10 суток', () => {
 		expect(commission(flyDate(9)).toBe(20));
 	});
 });
@@ -21,7 +21,7 @@ describe('возврат от 20% до 50%', () => {
 	test('время возврата = 5 суток', () => {
 		expect(commission(flyDate(5)).toBe(50));
 	});
-		test('время возврата < 5 суток', () => {
+	test('время возврата < 5 суток', () => {
 		expect(commission(flyDate(4)).toBe(50));
 	});
 });
@@ -33,7 +33,7 @@ describe('возврат от 50% до 75%', () => {
 	test('время возврата = 1 сутки', () => {
 		expect(commission(flyDate(1)).toBe(50));
 	});
-		test('время возврата < 1 суток', () => {
+	test('время возврата < 1 суток', () => {
 		expect(commission(flyDate(0.9)).toBe(75));
 	});
 });
@@ -45,7 +45,7 @@ describe('возврат от 75% до 100%', () => {
 	test('время возврата = времени вылета', () => {
 		expect(commission(flyDate(0)).toBe(75));
 	});
-		test('время возврата < времени вылета', () => {
+	test('время возврата < времени вылета', () => {
 		expect(commission(flyDate(-1)).toBe(100));
 	});
 });
