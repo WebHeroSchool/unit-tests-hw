be82a5d10e2bad7a0d3d7cdacd1cdbc0f1419906
Anton <chiga2030@gmail.com>
2022-01-21 14:58:42 +0300

    added descrition for test cases

--- __tests__/rejectOnTimeout.test.js
+++ __tests__/rejectOnTimeout.test.js
@@ -2,46 +2,76 @@ const { rejectOnTimeout } = require('../rejectOnTimeout');
 
 
 /**
+* Имеем 2 промиса: передаваемый и возвращаемый.
 *
+* Будем проверять состояния завершения каждого из них.
 *
+* Разделим проверку на следующие классы:
+* 1 - передаваемый завершился успехом
+* 2 - передаваемый завершился ошибкой
+* 3 - возвращаемый завершиля успехом
+* 4 - возвращаемый завершился ошибкой
 *
+* Критери определения успеха/ошибки:
+* Передаваемый промис успел завершитя до
+* того, как истек таймаут возвращаемого.
 *
-*
-*
-*
+* Обозначим параметр { msForMockPromise }
+* как величину времени, нужную для выполнения
+* передаваемого промиса.
+* Границы эквивалентности проведем относительно
+* параметра { msForMockPromise },
+* и обозначим их, как:
+* { msSucessPromise } >= { msForMockPromise }
+* { msFailurePromise } < { msForMockPromise }
 */
 
 
+const msForMockPromise = 300;
+const msSucessPromise = msForMockPromise;
+const msFailurePromise = msForMockPromise - 1;
+
+// передавемый промис, возвращает успех:
 const mockPromiseResolve = (resolve, reject) => {
-  setTimeout(() => resolve('sucessfull_test'), 300);
+  setTimeout(() => resolve('sucessfull_test'), msForMockPromise);
 };
 
+// передавемый промис, возвращает провал:
 const mockPromiseReject = (resolve, reject) => {
-  setTimeout(() => reject('test_failure'), 300);
+  setTimeout(() => reject('test_failure'), msForMockPromise);
 };
 
+// фабрика промисов, что бы результаты тестов не влияли друг на друга:
 const newPromise = promiseBody => new Promise(promiseBody);
 
 
-describe('describe - ', () => {
-  test('Ожидаем что мокковый промис перейдет в состояние fulfilled. В ответ получим Resolve моккового промиса (sucessfull_test).', () => {
-   	return expect(rejectOnTimeout(newPromise(mockPromiseResolve), 500))
+describe('Тестируем передаваемые промисы:', () => {
+  test('Ожидаем что передаваемый промис перейдет в состояние fulfilled. В ответ получим Resolve передаваемого промиса.', () => {
+   	return expect(rejectOnTimeout(newPromise(mockPromiseResolve), msSucessPromise))
    		.resolves.toBe('sucessfull_test');
   });
 
 
-  test('Ожидаем что возвращаемый промис перейдет в состояние rejected. В ответ получим Reject  возвращаемого промиса (timeout_error).', () => {
-    return expect(rejectOnTimeout(newPromise(mockPromiseResolve), 100))
-      .rejects.toBe('timeout_error')
-
-  	expect.assertions(1);
-  });
-
-
-  test('Ожидаем что мокковый промис перейдет в состояние rejected. В ответ получим Reject  моккового промиса (test_failure).', () => {
-  	return expect(rejectOnTimeout(newPromise(mockPromiseReject), 500))
+  test('Ожидаем что передаваемый промис перейдет в состояние rejected. В ответ получим Reject  передаваемого промиса.', () => {
+  	return expect(rejectOnTimeout(newPromise(mockPromiseReject), msSucessPromise))
       .rejects.toBe('test_failure');
 
   	expect.assertions(1);
+  });
+});
+
+
+describe('Тестируем возвращаемые промисы:', () => {
+  test('Ожидаем что возвращаемый промис перейдет в состояние fulfilled. В ответ получим Resolve возвращаемого промиса.', () => {
+    return expect(rejectOnTimeout(newPromise(mockPromiseResolve), msSucessPromise))
+      .resolves.toBe('sucessfull_test')
+  });
+
+
+  test('Ожидаем что возвращаемый промис перейдет в состояние rejected. В ответ получим Reject возвращаемого промиса.', () => {
+    return expect(rejectOnTimeout(newPromise(mockPromiseResolve), msFailurePromise))
+      .rejects.toBe('timeout_error')
+
+    expect.assertions(1);
   });
 });
